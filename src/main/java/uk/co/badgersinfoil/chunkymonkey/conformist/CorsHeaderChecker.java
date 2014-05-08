8911089b818a00dfeacf741822ff893d4e3bc509
David Holroyd <dave@badgers-in-foil.co.uk>
2014-05-08 08:39:44 +0100

    One report of missing headers; not one per header

--- src/main/java/uk/co/badgersinfoil/chunkymonkey/conformist/CorsHeaderChecker.java
+++ src/main/java/uk/co/badgersinfoil/chunkymonkey/conformist/CorsHeaderChecker.java
@@ -1,7 +1,9 @@
 package uk.co.badgersinfoil.chunkymonkey.conformist;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.protocol.HttpClientContext;
@@ -39,20 +41,24 @@ public class CorsHeaderChecker implements HttpResponseChecker {
 
 	@Override
 	public void check(Locator loc, HttpResponse resp, HttpClientContext ctx) {
-		checkHeaderElements(loc, resp, "Access-Control-Allow-Headers", EXPECTED_ALLOW_HEADERS);
-		checkHeaderElements(loc, resp, "Access-Control-Expose-Headers", EXPECTED_EXPOSE_HEADERS);
-		checkHeaderElements(loc, resp, "Access-Control-Allow-Methods", EXPECTED_ALLOW_METHODS);
-		checkHeaderElements(loc, resp, "Access-Control-Allow-Origin", EXPECTED_ALLOW_ORIGIN);
+		List<String> missing = new ArrayList<String>();
+		checkHeaderElements(loc, resp, "Access-Control-Allow-Headers", EXPECTED_ALLOW_HEADERS, missing);
+		checkHeaderElements(loc, resp, "Access-Control-Expose-Headers", EXPECTED_EXPOSE_HEADERS, missing);
+		checkHeaderElements(loc, resp, "Access-Control-Allow-Methods", EXPECTED_ALLOW_METHODS, missing);
+		checkHeaderElements(loc, resp, "Access-Control-Allow-Origin", EXPECTED_ALLOW_ORIGIN, missing);
+		if (!missing.isEmpty()) {
+			rep.carp(loc, "Missing CORS headers: %s", missing);
+		}
 	}
 
-	private void checkHeaderElements(Locator loc, HttpResponse resp, String headerName, Set<String> expectedElements) {
+	private void checkHeaderElements(Locator loc, HttpResponse resp, String headerName, Set<String> expectedElements, List<String> missing) {
 		if (resp.containsHeader(headerName)) {
 			Set<String> allowHeaders = HeaderUtil.getMergedHeaderElements(resp, headerName);
 			if (!expectedElements.equals(allowHeaders)) {
 				rep.carp(loc, "%s header values not to spec: ", headerName, allowHeaders);
 			}
 		} else {
-			rep.carp(loc, "Missing %s header", headerName);
+			missing.add(headerName);
 		}
 	}
 }
