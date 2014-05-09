e9fb62f95374c2e6f16ba734ae93e7309c862be2
David Holroyd <dave@badgers-in-foil.co.uk>
2014-05-09 21:14:46 +0100

    Don't fail on Transport Stream files > 2GB

--- src/main/java/uk/co/badgersinfoil/chunkymonkey/ts/FileTransportStreamParser.java
+++ src/main/java/uk/co/badgersinfoil/chunkymonkey/ts/FileTransportStreamParser.java
@@ -2,12 +2,10 @@ package uk.co.badgersinfoil.chunkymonkey.ts;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-
 import uk.co.badgersinfoil.chunkymonkey.Locator;
 
 public class FileTransportStreamParser {
@@ -20,19 +18,35 @@ public class FileTransportStreamParser {
 
 	public void parse(Locator locator, RandomAccessFile file) throws IOException {
 		FileChannel ch = file.getChannel();
-		MappedByteBuffer in = ch.map(FileChannel.MapMode.READ_ONLY, 0, ch.size());
-		ByteBuf buf = Unpooled.wrappedBuffer(in);
-
-		TSContext ctx = consumer.createContext(null);
-		long packetCount = ch.size() / TSPacket.TS_PACKET_LENGTH;
-		for (int packetNo=0; packetNo<packetCount ; packetNo++) {
-			ByteBuf pk = buf.slice(packetNo*TSPacket.TS_PACKET_LENGTH, TSPacket.TS_PACKET_LENGTH);
-			TSPacket packet = new TSPacket(locator, packetNo, pk);
-			if (!packet.synced()) {
-				// TODO: better diagnostics.  re-sync?
-				throw new RuntimeException("Transport stream synchronisation lost @packet#"+packetNo+" in "+locator);
+		long size = ch.size();
+		long offset=0, mapped=0;
+		long packetNo = 0;
+		TSContext fileContext = new TSContext() {  /* TODO: anything? */  };
+		TSContext ctx = consumer.createContext(fileContext);
+		while (true) {
+			final long remaining = size - offset;
+			if (remaining == 0) {
+				break;
 			}
-			consumer.packet(ctx, packet);
+			if (remaining > Integer.MAX_VALUE) {
+				mapped = Integer.MAX_VALUE - Integer.MAX_VALUE % TSPacket.TS_PACKET_LENGTH;
+			} else {
+				mapped = remaining;
+			}
+			MappedByteBuffer in = ch.map(FileChannel.MapMode.READ_ONLY, offset, mapped);
+			ByteBuf buf = Unpooled.wrappedBuffer(in);
+
+			long packetCount = mapped / TSPacket.TS_PACKET_LENGTH;
+			for (int i=0; i<packetCount ; i++) {
+				ByteBuf pk = buf.slice(i*TSPacket.TS_PACKET_LENGTH, TSPacket.TS_PACKET_LENGTH);
+				TSPacket packet = new TSPacket(locator, packetNo++, pk);
+				if (!packet.synced()) {
+					// TODO: better diagnostics.  re-sync?
+					throw new RuntimeException("Transport stream synchronisation lost @packet#"+i+" in "+locator);
+				}
+				consumer.packet(ctx, packet);
+			}
+			offset += mapped;
 		}
 	}
 }
