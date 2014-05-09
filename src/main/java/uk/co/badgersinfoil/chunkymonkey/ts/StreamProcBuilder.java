f4d5c502b2c2471f450c44594c037ac444875881
David Holroyd <dave@badgers-in-foil.co.uk>
2014-05-09 22:08:47 +0100

    Remove dead code

--- src/main/java/uk/co/badgersinfoil/chunkymonkey/ts/StreamProcBuilder.java
+++
@@ -1,8 +0,0 @@
-package uk.co.badgersinfoil.chunkymonkey.ts;
-
-import uk.co.badgersinfoil.chunkymonkey.ts.ProgramMapTable.StreamDescriptorIterator;
-
-public interface StreamProcBuilder {
-
-	TSPacketConsumer create(StreamDescriptorIterator i);
-}
