npanton/chunkymonkey

commit 761462b9d6d72c28141ed3e24ba9aa396dae131a

Don't start stats API server if run time-limited

src/main/java/uk/co/badgersinfoil/chunkymonkey/conformist/Main.java
@@ -67,10 +67,10 @@ public class Main {
 				b.hls(true);
 				HlsMasterPlaylistProcessor processor = b.buildSingle(scheduledExecutor, rep);
 				HlsMasterPlaylistContext ctx = processor.createContext(uri);
-				Server server = ServerBuilder.create(ctx).build();
-				processor.start(ctx);
-				server.start();
 				if (timeLimit == null) {
+					Server server = ServerBuilder.create(ctx).build();
+					processor.start(ctx);
+					server.start();
 					while (true) {
 						Thread.sleep(10_000);
 					}

commit f12ae160d01d2c4d5d5f0948622ce43cfd6da696

Still start processing if time-limit is enabled
Fixes regression introduced by 761462b9d6d72c28141ed3e24ba9aa396dae131a

src/main/java/uk/co/badgersinfoil/chunkymonkey/conformist/Main.java
@@ -75,6 +75,7 @@ public class Main {
 						Thread.sleep(10_000);
 					}
 				} else {
+					processor.start(ctx);
 					Thread.sleep(timeLimit * 1000);
 				}
 				System.err.println("Shutting down HLS processing");